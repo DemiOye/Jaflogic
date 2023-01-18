75057bd149f20a9c357ca051871f2eb803abc2c9
DemiOye <demi.oyeyele@gmail.com>
2023-01-18 23:24:37 +0100

    First commit

--- index.js
+++ index.js
@@ -1,34 +1,47 @@
 
 let slide = document.getElementById("home");
-
-
+let dot1 = document.getElementById("dot-1");
+let dot2 = document.getElementById("dot-2");
+let dot3 = document.getElementById("dot-3");
 
 window.onload = function() {
 
     slide.className = "slide-one";
+    dot1.className = "dot-active";
+    dot2.className = "dot-inactive";
+    dot3.className = "dot-inactive";
 
     function slideShow() {
 
-            if (slide.className === "slide-one") {
+        if (slide.className === "slide-one") {
 
                 slide.className = "slide-two";
+                dot1.className = "dot-inactive";
+                dot2.className = "dot-active";
+                dot3.className = "dot-inactive";
 
                 //console.log(slide.className);
         }
 
-        else    if (slide.className === "slide-two") {
+        else if (slide.className === "slide-two") {
 
-                    slide.className = "slide-three";
+                slide.className = "slide-three";
+                dot1.className = "dot-inactive";
+                dot2.className = "dot-inactive";
+                dot3.className = "dot-active";
 
-                    //console.log(slide.className);
+                //console.log(slide.className);
 
             }
 
-        else    if (slide.className === "slide-three") {
+        else if (slide.className === "slide-three") {
 
-                    slide.className = "slide-one";
+                slide.className = "slide-one";
+                dot1.className = "dot-active";
+                dot2.className = "dot-inactive";
+                dot3.className = "dot-inactive";
 
-                    //console.log(slide.className);
+                //console.log(slide.className);
 
             }
 
